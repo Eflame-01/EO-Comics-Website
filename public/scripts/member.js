70911e91c8dbdd1ce972b2be44fbb9430c7e6f9d
Ese <eomene2@uic.edu>
2022-07-30 13:02:11 -0500

    Add Logoff Feature

--- public/scripts/member.js
+++ public/scripts/member.js
@@ -4,25 +4,33 @@
   login/sign up
 */
 
-// localStorage.removeItem("memberData"); //place holder text to clear local storage of user data
-
+//================= GLOBAL VARIABLES ===========================//
 var data = localStorage.getItem("memberData"); //global variable that has user's login and some subscription data
 
-if(data != null || data != undefined){
-  //parse data
-  data = JSON.parse(data);
+//================= ON READY FUNCTIONS =========================//
+$(document.body).ready(function(){
+  if(data != null || data != undefined){
+    //parse data
+    data = JSON.parse(data);
 
-  //hide sign-in/sign-up features
-  $("#sign-up-login-information").attr("hidden", "true");
+    //hide sign-in/sign-up features
+    $("#sign-up-login-information").attr("hidden", "true");
 
-  //show welcome [username] text and settings dropdown
-  $("#member-information").removeAttr("hidden");
-  $('p[name$="welcome-user-sign"]').text("Welcome " + data.merge_fields.USERNAME);
-}
-else{
-  //show sign in/sign up features
-  $("#sign-up-login-information").removeAttr("hidden");
+    //show welcome [username] text and settings dropdown
+    $("#member-information").removeAttr("hidden");
+    $('p[name$="welcome-user-sign"]').text("Welcome " + data.merge_fields.USERNAME);
+  }
+  else{
+    //show sign in/sign up features
+    $("#sign-up-login-information").removeAttr("hidden");
 
-  //hide welcome [username] text and settings dropdown
-  $("#member-information").attr("hidden", "true");
-}
+    //hide welcome [username] text and settings dropdown
+    $("#member-information").attr("hidden", "true");
+  }
+});
+
+//================ ON CLICK FUNCTIONS ==========================//
+$("a[name$=logoff-link]").click(function(){
+  localStorage.removeItem("memberData");
+  window.location.replace("index.html"); //TODO: Try to refresh page if it gives same result
+});
